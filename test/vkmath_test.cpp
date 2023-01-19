4cce9ff9c38a22c72033a069502a120b1b72c1b6
miles <mm1987b3@gmail.com>
2023-01-18 21:20:32 -0500

    added additional matrix multiplication test cases
    looks like you'll have to be very careful when setting out the block
    size that "x" controls the columns and "y" controls the rows for memory
    access coalescence (I think)

--- test/vkmath_test.cpp
+++ test/vkmath_test.cpp
@@ -25,63 +25,72 @@
 
 #define TILE_SIZE 16
 
+void run_vkgpu_test(u32 m, u32 k, u32 n)
+{
+  compute_pipeline vkmatmul("compute_gemm.spv", 6);
+
+  std::mt19937 mt(42);
+  std::uniform_real_distribution<float> dst(0., 1.);
+
+  array<float> A(m * k);
+  array<float> B(k * n);
+  array<float> Ccpu(m * n);
+  array<float> Cgpu(m * n);
+
+  for (u32 i = 0; i < m * k; ++i)
+    A[i] = dst(mt);
+  for (u32 i = 0; i < k * n; ++i)
+    B[i] = dst(mt);
+  for (u32 i = 0; i < m * n; ++i)
+  {
+    Ccpu[i] = 0.f;
+    Cgpu[i] = 0.f;
+  }
+
+  buffer_set<u32> d_m(&vkmatmul.dset, 0);
+  buffer_set<u32> d_k(&vkmatmul.dset, 1);
+  buffer_set<u32> d_n(&vkmatmul.dset, 2);
+  buffer_set<float> d_A(&vkmatmul.dset, 3);
+  buffer_set<float> d_B(&vkmatmul.dset, 4);
+  buffer_set<float> d_C(&vkmatmul.dset, 5);
+
+  d_m.update(&m, 1);
+  d_k.update(&k, 1);
+  d_n.update(&n, 1);
+  d_A.update(A.data, A.len);
+  d_B.update(B.data, B.len);
+  d_C.update(Cgpu.data, Cgpu.len);  // don't like that you have to do this
+                                    // seems like this issue stems from malloc
+                                    // and copy being the same operation
+
+  matmul(m, k, n, 1.f, A.data, B.data, 0.f, Ccpu.data);
+
+  vkmatmul.run((n + (TILE_SIZE - 1)) / TILE_SIZE,
+               (m + (TILE_SIZE - 1)) / TILE_SIZE, 1);
+
+  d_C.retrieve(Cgpu.data);
+
+  for (u64 i = 0; i < m * n; ++i)
+  {
+    EXPECT_FLOAT_EQ(Ccpu[i], Cgpu[i], 10);
+  }
+}
+
 TEST(vkmath_test_matmul, 1)
 {
   vkinit(false);
 
   {
-    u32 N = 100;
-
     graphics_pipeline scene_pipeline;
     graphics_pipeline ui_pipeline;
     make_swap_chain_dependencies(scene_pipeline, ui_pipeline);
 
-    compute_pipeline vkmatmul("compute_gemm.spv", 6);
-
-    std::mt19937 mt(42);
-    std::uniform_real_distribution<float> dst(0., 1.);
-
-    array<float> A(N * N);
-    array<float> B(N * N);
-    array<float> Ccpu(N * N);
-    array<float> Cgpu(N * N);
-
-    for (u32 r = 0; r < N; ++r)
-    {
-      for (u32 c = 0; c < N; ++c)
-      {
-        A[r * N + c]    = dst(mt);
-        B[r * N + c]    = dst(mt);
-        Ccpu[r * N + c] = 0.f;
-        Cgpu[r * N + c] = 0.f;
-      }
-    }
-
-    buffer_set<u32> d_m(&vkmatmul.dset, 0);
-    buffer_set<u32> d_k(&vkmatmul.dset, 1);
-    buffer_set<u32> d_n(&vkmatmul.dset, 2);
-    buffer_set<float> d_A(&vkmatmul.dset, 3);
-    buffer_set<float> d_B(&vkmatmul.dset, 4);
-    buffer_set<float> d_C(&vkmatmul.dset, 5);
-
-    d_m.update(&N, 1);
-    d_k.update(&N, 1);
-    d_n.update(&N, 1);
-    d_A.update(A.data, A.len);
-    d_B.update(B.data, B.len);
-    d_C.update(Cgpu.data, Cgpu.len);  // don't like that you have to do this
-                                      // seems like this issue stems from malloc
-                                      // and copy being the same operation
-
-    matmul(N, N, N, 1.f, A.data, B.data, 0.f, Ccpu.data);
-
-    vkmatmul.run((N + (TILE_SIZE - 1)) / TILE_SIZE,
-                 (N + (TILE_SIZE - 1)) / TILE_SIZE, 1);
-
-    d_C.retrieve(Cgpu.data);
-
-    for (u64 i = 0; i < N * N; ++i)
-      EXPECT_FLOAT_EQ(Ccpu[i], Cgpu[i], 10);
+    run_vkgpu_test(128, 128, 128);  // square, tile divides evenly
+    run_vkgpu_test(100, 100, 100);  // square, tile divides unevenly
+    run_vkgpu_test(128, 128, 150);  // short and fat C, inner dim divs evenly
+    run_vkgpu_test(150, 128, 128);  // tall and skinny C, inner dim divs evenly
+    run_vkgpu_test(128, 175, 150);  // short and fat C, inner dim divs unevenly
+    run_vkgpu_test(150, 175, 128);  // tall n skinny C, inner dim divs unevenly
   }
 
   clean();
